e00e0177451b2b2c496829849c4315852957fdec
Si Wei Yang <yangszwei@outlook.com>
2024-04-24 15:44:49 +0800

    fix(ui): Fix minor issue in Header component

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -21,7 +21,7 @@
 	@component
 	Base header for the application.
 -->
-<header class="z-10 flex h-20 shrink-0 items-center gap-3 bg-green-500 px-3 shadow">
+<header class="z-50 flex h-20 shrink-0 items-center gap-3 bg-green-500 px-3 shadow">
 	<!-- App Title -->
 	<a href="{base}/" target="_self" class="flex items-center gap-2 text-2xl text-white" on:click={gotoHome}>
 		<img src="{assets}/favicon.png" class="h-16 w-16 object-contain" alt="" />
