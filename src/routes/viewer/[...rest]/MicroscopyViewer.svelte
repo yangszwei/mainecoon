85b5f8dbea3ba589110ba999bc1e0be69b455348
Si Wei Yang <yangszwei@outlook.com>
2024-05-03 00:08:42 +0800

    feat: Minor improvements in MicroscopyViewer
    - Make annotations load asynchronously
    - Add loading animation
    - Add basic error handling
    - Add mouse position & overview map control

--- src/routes/viewer/[...rest]/MicroscopyViewer.svelte
+++ src/routes/viewer/[...rest]/MicroscopyViewer.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
+	import 'ol/ol.css';
+	import { OverviewMap, defaults as defaultControls } from 'ol/control';
 	import { Map } from 'ol';
+	import MousePosition from 'ol/control/MousePosition.js';
+	import TileLayer from 'ol/layer/Tile';
 	import VectorLayer from 'ol/layer/Vector';
 	import VectorSource from 'ol/source/Vector';
 	import { computeAnnotationFeatures } from '$lib/microscopy-viewer/annotation';
@@ -18,25 +22,64 @@
 
 	export let annotations: AnnotationInfo[] | undefined;
 
-	onMount(async () => {
-		const { extent, layer, resolutions, view } = computePyramidInfo(baseUrl, studyUid, seriesUid, images);
-		const features = await computeAnnotationFeatures(annotations, resolutions);
+	let loading = true;
+	let errorMessage: string | undefined;
 
-		const map = new Map({
-			controls: [],
-			target: 'map',
-			layers: [
-				layer,
-				new VectorLayer({
-					source: new VectorSource({ features }),
-					extent,
-				}),
-			],
-			view,
-		});
+	onMount(() => {
+		if (images.length === 0) {
+			loading = false;
+			errorMessage = 'No images found.';
+			return;
+		}
 
-		map.getView().fit(extent, { size: map.getSize() });
+		let map: Map;
+
+		try {
+			const { extent, layer, resolutions, view } = computePyramidInfo(baseUrl, studyUid, seriesUid, images);
+
+			map = new Map({
+				controls: defaultControls().extend([
+					new MousePosition({
+						coordinateFormat: (c: number[] | undefined) => (c ? `${c[0].toFixed(2)}, ${-c[1].toFixed(2)}` : ''),
+						className: 'm-1.5 text-center text-sm',
+					}),
+					new OverviewMap({
+						layers: [new TileLayer({ source: layer.getSource() ?? undefined })],
+					}),
+				]),
+				target: 'map',
+				layers: [layer],
+				view,
+			});
+
+			computeAnnotationFeatures(annotations, resolutions)
+				.then((features) => {
+					if (features.length > 0) {
+						const source = new VectorSource({ features });
+						map.addLayer(new VectorLayer({ source, extent }));
+					}
+				})
+				.catch((error) => {
+					errorMessage = 'Failed to load annotations.';
+					console.error(error); // eslint-disable-line no-console
+				})
+				.finally(() => (loading = false));
+
+			map.getView().fit(extent, { size: map.getSize() });
+		} catch (error) {
+			errorMessage = 'Unexpected error occurred.';
+			loading = false;
+			console.error(error); // eslint-disable-line no-console
+		}
 	});
 </script>
 
-<div id="map" class="h-full w-full {$$props.class ?? ''}" />
+<div class="relative h-full w-full {$$props.class ?? ''}">
+	<div id="map" class="h-full w-full" />
+	<div class="absolute inset-0 z-10 flex items-center justify-center bg-black/40" class:hidden={!loading}>
+		<span class="loader border-green-500" />
+	</div>
+	<div class="absolute inset-0 z-10 flex items-center justify-center bg-black/40" class:hidden={!errorMessage}>
+		<p>{errorMessage}</p>
+	</div>
+</div>
