f77ba7b003a7a1248c4570ff89af7e35157f0052
Si Wei Yang <yangszwei@outlook.com>
2024-07-04 15:06:19 +0800

    feat: Make Drawer component resizable

--- src/app/_components/Drawer.tsx
+++ src/app/_components/Drawer.tsx
@@ -1,8 +1,11 @@
 'use client';
 
+import type { MouseEvent, ReactNode } from 'react';
+import { useEffect, useRef } from 'react';
 import { Icon } from '@iconify/react';
-import type { ReactNode } from 'react';
 import mdiChevronDown from '@iconify-icons/mdi/chevron-down';
+import mdiChevronLeft from '@iconify-icons/mdi/chevron-left';
+import mdiChevronRight from '@iconify-icons/mdi/chevron-right';
 import { useState } from 'react';
 
 export interface DrawerSectionProps {
@@ -34,6 +37,70 @@ export function DrawerSection({ title, open, fixed, children }: Readonly<DrawerS
 	);
 }
 
+function useResize(placement: 'left' | 'right' | undefined, widths: number[], hideThreshold: number) {
+	const [isResizing, setIsResizing] = useState(false);
+	const [defaultWidth, minWidth, maxWidth] = widths;
+	const [width, setWidth] = useState(defaultWidth);
+	const [isHidden, setIsHidden] = useState(false);
+
+	useEffect(() => {
+		const handleMouseMove = (e: MouseEvent) => {
+			if (!isResizing) return;
+
+			let newWidth = placement === 'right' ? window.innerWidth - e.clientX : e.clientX;
+
+			// Snap to default width if close enough
+			if (Math.abs(newWidth - defaultWidth) < 5) {
+				newWidth = defaultWidth;
+			}
+
+			// Hide drawer if width is less than hideThreshold
+			if (newWidth < hideThreshold) {
+				setIsHidden(true);
+				setWidth(0);
+			} else {
+				setIsHidden(false);
+				setWidth(Math.max(minWidth, Math.min(maxWidth, newWidth)));
+			}
+		};
+
+		const handleMouseUp = () => {
+			setIsResizing(false);
+			document.documentElement.style.cursor = '';
+			document.removeEventListener('mousemove', handleMouseMove as never);
+			document.removeEventListener('mouseup', handleMouseUp);
+		};
+
+		if (isResizing) {
+			document.documentElement.style.cursor = 'ew-resize';
+			document.addEventListener('mousemove', handleMouseMove as never);
+			document.addEventListener('mouseup', handleMouseUp);
+		}
+
+		return () => {
+			document.removeEventListener('mousemove', handleMouseMove as never);
+			document.removeEventListener('mouseup', handleMouseUp);
+		};
+	}, [isResizing, placement, defaultWidth, minWidth, maxWidth, hideThreshold]);
+
+	const handleMouseDown = (e: MouseEvent) => {
+		e.preventDefault();
+		setIsResizing(true);
+	};
+
+	const toggleDrawer = () => {
+		if (isHidden) {
+			setIsHidden(false);
+			setWidth(defaultWidth);
+		} else {
+			setIsHidden(true);
+			setWidth(0);
+		}
+	};
+
+	return [isHidden, width, handleMouseDown, toggleDrawer] as const;
+}
+
 export interface DrawerProps {
 	className?: string;
 	placement?: 'left' | 'right';
@@ -42,14 +109,31 @@ export interface DrawerProps {
 
 /** The drawer component (a.k.a. sidebar) provides access to secondary content. */
 export default function Drawer({ className, placement, children }: Readonly<DrawerProps>) {
+	const drawerRef = useRef(null);
+	const [isHidden, width, handleMouseDown, toggleDrawer] = useResize(placement, [320, 288, 352], 16);
+
 	// Default placement is 'left'.
 	const placementClass = placement === 'right' ? 'border-l' : 'border-r';
 
 	return (
-		<aside
-			className={`relative h-full w-80 shrink-0 overflow-auto bg-white shadow-sm ${placementClass} ${className || ''}`}
-		>
-			{children}
+		<aside ref={drawerRef} className={`relative h-full shrink-0 ${placementClass} ${className || ''}`}>
+			{isHidden ? (
+				<div className={`flex h-full flex-col justify-center bg-white ${placementClass}`} onMouseDown={handleMouseDown}>
+					<button type="button" onClick={toggleDrawer}>
+						<Icon icon={placement === 'right' ? mdiChevronLeft : mdiChevronRight} className="h-4 w-4" />
+					</button>
+				</div>
+			) : (
+				<>
+					<div className="h-full overflow-auto bg-white shadow-sm" style={{ width }}>
+						{children}
+					</div>
+					<div
+						onMouseDown={handleMouseDown}
+						className={`absolute top-0 ${placement === 'right' ? '-left-1' : '-right-1'} h-full w-2 cursor-ew-resize`}
+					/>
+				</>
+			)}
 		</aside>
 	);
 }
